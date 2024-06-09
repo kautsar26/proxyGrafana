1db9d51f0e0efd78baff9e724e933fe0ea8e4b14
kautsar26 <muhammadalkautsr@gmail.com>
2024-06-09 17:01:31 +0700

    second commit

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 81;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 // Middleware untuk mengizinkan CORS (Cross-Origin Resource Sharing)
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); // Mengizinkan metode HTTP yang diizinkan
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Mengizinkan header yang diizinkan
     next();
 });
 
-app.get('/dashboard/:dashboardId', async (req, res) => {
+app.get('/public-dashboards/:dashboardId', async (req, res) => {
     const { dashboardId } = req.params;
     try {
         // Ganti URL dengan URL public dashboard Anda
